c286aad5101c7e2ba4a390fb682cc43980e999b9
Grzegorz Matyszewski <greg.matyszewski@gmail.com>
2021-04-06 18:30:08 +0200

    tabs

--- src/ts/components/Stats.ts
+++ src/ts/components/Stats.ts
@@ -6,6 +6,9 @@ export class Stats extends Component {
 
     private $tab: JQuery;
     private $item: JQuery;
+    private $wrap: JQuery;
+    private $current: JQuery;
+    private tabToShow: number; // for async switch
 
 
 
@@ -14,6 +17,7 @@ export class Stats extends Component {
 
         this.$tab = this.view.find('[data-tab]');
         this.$item = this.view.find('[data-view]');
+        this.$wrap = this.view.find('.js-tabs-wrapper');
 
         this.bind();
         this.setActiveView(2);
@@ -22,12 +26,12 @@ export class Stats extends Component {
 
 
     private bind(): void {
-        this.$tab.off('.tab').on('click.tab', this.switchTab);
+        this.$tab.off('.tab').on('click.tab', this.onTabClick);
     }
 
 
 
-    private switchTab = (e): void => {
+    private onTabClick = (e): void => {
         const current = $(e.currentTarget);
         const index = current.data('tab');
         this.setActiveView(index);
@@ -36,10 +40,47 @@ export class Stats extends Component {
 
 
     private setActiveView(index: number): void {
+        this.tabToShow = index;
         this.$tab.removeClass('is-active');
-        this.$item.removeClass('is-active');
         this.$tab.filter('[data-tab=' + index + ']').addClass('is-active');
-        this.$item.filter('[data-view=' + index + ']').addClass('is-active');
-        $window.resize();
+        this.hideCurrent().then(() => {
+            this.show(this.tabToShow);
+            this.tabToShow = null;
+            $window.resize();
+        });
+    }
+
+
+
+    private hideCurrent(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            if (!this.$current) { resolve(); return; }
+            gsap.to(this.$current, {
+                opacity: 0,
+                duration: 0.3,
+                ease: 'sine',
+                onComplete: () => {
+                    this.$current.removeClass('is-active');
+                    resolve();
+                },
+            });
+        })
+    }
+
+
+
+    private show(index: number): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this.$current = this.$item.filter('[data-view=' + index + ']');
+            this.$current.addClass('is-active');
+            gsap.fromTo(this.$current, {
+                opacity: 0,
+            }, {
+                opacity: 1,
+                duration: 0.7,
+                ease: 'sine',
+                onComplete: () => resolve(),
+            });
+        })
     }
 }
